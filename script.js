292ff27654b4cd0555ea396e68df608dce7325ef
gabriel M <gabriel_jm_3@hotmail.com>
2024-05-25 16:56:29 -0300

    nuevo commit gabo

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-        const navbarLinks = document.getElementById('navbar-links');
-
-        hamburger.addEventListener('click', () => {
-            hamburger.classList.toggle('active');
-            navbarLinks.classList.toggle('active');
-        });
