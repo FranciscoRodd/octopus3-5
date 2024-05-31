7d88f597777a3afb41cf7ad72af45d88dbf4c11c
gabriel M <gabriel_jm_3@hotmail.com>
2024-05-31 12:50:11 -0300

    nuevo commit

--- script.js
+++ script.js
@@ -5,3 +5,44 @@ const hamburger = document.getElementById('hamburger');
             hamburger.classList.toggle('active');
             navbarLinks.classList.toggle('active');
         });
+
+
+    // Importar jQuery y Slick Carousel
+import $ from 'jquery';
+import 'slick-carousel';
+
+// Código para inicializar Slick Carousel
+$(document).ready(function(){
+  $('.your-class').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
