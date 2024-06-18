32a159bd82e4150b155a5b8809d8079225069698
gabriel M <gabriel_jm_3@hotmail.com>
2024-06-18 03:00:53 -0300

    gabo commit

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const hamburger = document.getElementById('hamburger');
 const navbarLinks = document.getElementById('navbar-links');
+const overlay = document.getElementById('overlay');
+const body = document.body;
+const navbar = document.querySelector('.navbar');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navbarLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    navbarLinks.classList.toggle('active');
+    overlay.style.display = navbarLinks.classList.contains('active') ? 'block' : 'none';
+    body.classList.toggle('no-scroll', navbarLinks.classList.contains('active'));
 });
+
+overlay.addEventListener('click', () => {
+    navbarLinks.classList.remove('active');
+    hamburger.classList.remove('active');
+    overlay.style.display = 'none';
+    body.classList.remove('no-scroll');
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
+
 
 
 $(document).ready(function () {
@@ -12,6 +33,7 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 2000,
     centerMode: true,
+    arrows: false,
     centerPadding: '60px',
     slidesToShow: 5,
     responsive: [
